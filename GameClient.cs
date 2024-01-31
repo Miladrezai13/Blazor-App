d28b37d97ddbe838f5b7a0a4a4aeb3239e247957
Milad Rezai88 <miladrezai13@gmail.com>
2024-01-31 18:30:19 +0100

    add new game

---
+++ GameClient.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GameStore.Models;
+
+namespace GameStore
+{
+    public static class  GameClient
+    {
+        private static readonly List <Game> games = new ()
+    {
+            new Game()
+            {
+                Id = 1, 
+                Name = "Street Fighter II",
+                Genre = "Fightning",
+                Price = 10.90M,
+                ReleaseDate = new DateTime (1991, 2, 2)
+            },
+            new Game()
+            {
+                Id = 2, 
+                Name = "Final Fantasy XIV",
+                Genre = "Roleplaying ",
+                Price = 10.90M,
+                ReleaseDate = new DateTime (1991, 2, 2)
+            },
+            new Game()
+            {
+                Id = 3, 
+                Name = "FiFA 23",
+                Genre = "Sport",
+                Price = 15.90M,
+                ReleaseDate = new DateTime (1991, 2, 2)
+            }
+
+    };
+    public static Game[] GetGames ()
+    {
+        return games.ToArray();
+    }
+    public static void AddGame(Game game)
+    {
+        game.Id = games.Max(game=> game.Id)+1;
+        games.Add(game);
+    }
+
+    public static Game GetGames(int id)
+    {
+        return games.Find(game => game.Id ==id ) ?? throw new Exception ("Could not find game !");
+    }
+
+    
+    public static void UpdateGame(Game updatedGame)
+    {
+        Game existingGame = GetGames(updatedGame.Id);
+        existingGame.Name = updatedGame.Name;
+        existingGame.Genre = updatedGame.Genre;
+        existingGame.Price = updatedGame.Price;
+        existingGame.ReleaseDate = updatedGame.ReleaseDate;
+    }
+
+
+    }
+}
--- Models/Game.cs
+++ Models/Game.cs
@@ -2,14 +2,24 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
 
 namespace GameStore.Models
 {
     public class Game
     {
         public int Id { get; set; }
-        public required string Name { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(50)]
+        public required string Name { get; set; }=string.Empty;
+
+        [Required]
+        [StringLength(20)]
         public required string Genre { get; set; }
+
+        [Range(1, 100)]
         public decimal Price { get; set; }
         public DateTime ReleaseDate { get; set; }
 
